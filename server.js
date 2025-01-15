aaf2b96448438bd8fe56b8e828fa1fdec20c2cda
feiron <pawel.rogowski.praca@gmail.com>
2025-01-15 16:56:42 +0100

    corrected login logs

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ connectDb()
 			try {
 				const user = await UserController.loginUser(email, password);
 				return reply.status(200).send({
-					message: `Login successful ${user.daysLeft} days left`,
+					message: `Login successful ${user.totalSubscriptionDays} days left`,
 					user,
 				});
 			} catch (error) {
